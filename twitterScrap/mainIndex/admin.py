d401ebb171c05e2058148c60268265bb63cf3f5c
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2022-03-15 21:43:25 +0700

    ลบเจคเก่า

--- twitterScrap/mainIndex/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import mainIndex
-# Register your models here.
-class MainIndexAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'completed')
-
-admin.site.register(mainIndex, MainIndexAdmin)
--- twitterScrap/mainIndex/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpRequest,HttpResponse
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello From Main Index")
